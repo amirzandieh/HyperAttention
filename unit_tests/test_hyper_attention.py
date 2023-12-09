ba80b01b25eb65ee2091e1da38d7b8f04e550b4c
amir <amir.zed512@gmail.com>
2023-12-09 10:12:32 +0100

    unit test for hyper attention function

--- unit_tests/test_hyper_attention.py
+++ unit_tests/test_hyper_attention.py
@@ -25,112 +25,106 @@ def check_memory(new_cnt=-1):
 
 
 class MyTestCase(unittest.TestCase):
-    # def test_block_indexing(self):
-    #     dtype = torch.bfloat16
-    #
-    #     batch_size = 4
-    #     block_size = 512
-    #     dim = 128
-    #     head_size = 32
-    #     seq_len = 2048
-    #     sample_size = 0
-    #
-    #     query = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
-    #     key = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
-    #     key[:, :, :sample_size, :] *= 4.
-    #     value = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
-    #
-    #     q_buckets_idx = torch.randint(0, 64, (batch_size, head_size, seq_len), device='cuda')
-    #     k_buckets_idx = torch.randint(0, 64, (batch_size, head_size, seq_len), device='cuda')
-    #     _, query_sort_idx = torch.sort(q_buckets_idx, dim=2, stable=True)
-    #     _, key_sort_idx = torch.sort(k_buckets_idx, dim=2, stable=True)
-    #     check_memory()
-    #
-    #     # compute attention by presorting queries and sorting back the output attention
-    #     t0 = time.time()
-    #     query_sort_idx_inv = torch.argsort(query_sort_idx, dim=2, stable=True)
-    #     query_sorted = indexing(query, query_sort_idx)
-    #     key_sorted = indexing(key, key_sort_idx)
-    #     value_sorted = indexing(value, key_sort_idx)
-    #     query_split_per_block = query_sorted.view(-1, 1, block_size, dim)
-    #     key_split_per_block = key_sorted.view(-1, 1, block_size, dim)
-    #     value_split_per_block = value_sorted.view(-1, 1, block_size, dim)
-    #     attn_block, lse_block = flash_attn_func(query_split_per_block.transpose(1, 2),
-    #                                             key_split_per_block.transpose(1, 2),
-    #                                             value_split_per_block.transpose(1, 2))
-    #     check_memory()
-    #
-    #     attn_sample, lse_sample = flash_attn_func(query.transpose(1, 2),
-    #                                             key[:, :, :sample_size, :].transpose(1, 2),
-    #                                             value[:, :, :sample_size, :].transpose(1, 2))
-    #     attn_block = attn_block.transpose(1, 2)
-    #     attn_block = attn_block.view(batch_size, head_size, query_sorted.shape[2], -1)
-    #     attn_sample = attn_sample.transpose(1, 2)
-    #     lse_block = lse_block[:, :, :query_sorted.shape[2]]
-    #     lse_block = lse_block.view(batch_size, head_size, query_sorted.shape[2], -1)
-    #     attn = indexing(attn_block, query_sort_idx_inv)+attn_sample
-    #     lse_block = indexing(lse_block, query_sort_idx_inv).squeeze(-1)
-    #     lse = (lse_sample.exp() + lse_block.exp()).log().to(dtype=lse_block.dtype)
-    #     t1 = time.time()
-    #     check_memory()
-    #     print(lse[0, 0])
-    #     print('the runtime of torch permutation and indexing of queries:', t1-t0)
-    #
-    #     # torch lse computation
-    #     qk = query_split_per_block @ key_split_per_block.transpose(-1, -2) / math.sqrt(dim)
-    #     lse_block_torch = torch.logsumexp(qk, dim=-1, keepdim=True)
-    #     lse_block_torch = lse_block_torch.view(batch_size, head_size, query_sorted.shape[2], -1)
-    #     lse_block_torch = indexing(lse_block_torch, query_sort_idx_inv).squeeze(-1)
-    #     lse_sample_torch = torch.logsumexp(
-    #         query @ key[:, :, :sample_size, :].transpose(-1, -2) / math.sqrt(dim),
-    #         dim=-1,
-    #         keepdim=True
-    #     ).squeeze(-1)
-    #     lse_torch = (lse_sample_torch.exp() + lse_block_torch.exp()).log().to(dtype=lse_block_torch.dtype)
-    #     print('diff between lse with sample and without: ', (lse_block_torch - lse_torch).norm(), lse_torch.norm())
-    #     print('error flash attention:', (lse - lse_torch).norm(), lse_torch.norm())
-    #
-    #     # compute attention kernel which permutes queries in triton
-    #     check_memory(0)
-    #     t2 = time.time()
-    #     attn_triton, lse_triton = hyper_attn_func(
-    #         query.transpose(1, 2),
-    #         key.transpose(1, 2),
-    #         value.transpose(1, 2),
-    #         query_sort_idx.transpose(1, 2),
-    #         key_sort_idx.transpose(1, 2),
-    #         block_size,
-    #         sample_size,
-    #     )
-    #     check_memory()
-    #     attn_triton = attn_triton.transpose(1, 2)
-    #     check_memory()
-    #     t3 = time.time()
-    #     print('the runtime of triton permuting queries:', t3 - t2)
-    #
-    #     print(lse_triton[0, 0])
-    #     print((lse - lse_triton).norm(), lse.norm())
-    #
-    #     print('error blocked flash attention:', (lse_triton - lse_torch).norm(), lse_torch.norm())
-    #
-    #     # check if dimension of V can be different from that of Q and K
-    #     value_small = value[:, :, :, :dim//2].clone()
-    #     attn_triton_unequal_dim, lse_triton_unequal_dim = hyper_attn_func(
-    #         query.transpose(1, 2),
-    #         key.transpose(1, 2),
-    #         value_small.transpose(1, 2),
-    #         query_sort_idx.transpose(1, 2),
-    #         key_sort_idx.transpose(1, 2),
-    #         block_size,
-    #         sample_size,
-    #     )
-    #     attn_triton_unequal_dim = attn_triton_unequal_dim.transpose(1, 2)
-    #
-    #     print('testing unequal dimension for V compared to Q, K')
-    #     print((attn_triton[:, :, :, :dim//2] - attn_triton_unequal_dim).norm())
-    #     # assert torch.equal(lse_permute, lse)
-    #     # assert torch.equal(attn_permute, attn)
-    #     # self.assertEqual(True, False)  # add assertion here
+    def test_block_indexing(self):
+        dtype = torch.bfloat16
+
+        batch_size = 4
+        block_size = 512
+        dim = 128
+        head_size = 32
+        seq_len = 2048
+        sample_size = 128
+
+        query = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
+        key = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
+        key[:, :, :sample_size, :] *= 4.
+        value = torch.randn((batch_size, head_size, seq_len, dim), device='cuda', dtype=dtype)
+
+        q_buckets_idx = torch.randint(0, 64, (batch_size, head_size, seq_len), device='cuda')
+        k_buckets_idx = torch.randint(0, 64, (batch_size, head_size, seq_len), device='cuda')
+        _, query_sort_idx = torch.sort(q_buckets_idx, dim=2, stable=True)
+        _, key_sort_idx = torch.sort(k_buckets_idx, dim=2, stable=True)
+        check_memory()
+
+        # compute attention by presorting queries and sorting back the output attention
+        t0 = time.time()
+        query_sort_idx_inv = torch.argsort(query_sort_idx, dim=2, stable=True)
+        query_sorted = indexing(query, query_sort_idx)
+        key_sorted = indexing(key, key_sort_idx)
+        value_sorted = indexing(value, key_sort_idx)
+        query_split_per_block = query_sorted.view(-1, 1, block_size, dim)
+        key_split_per_block = key_sorted.view(-1, 1, block_size, dim)
+        value_split_per_block = value_sorted.view(-1, 1, block_size, dim)
+        attn_block, lse_block = flash_attn_func(query_split_per_block.transpose(1, 2),
+                                                key_split_per_block.transpose(1, 2),
+                                                value_split_per_block.transpose(1, 2))
+
+        attn_sample, lse_sample = flash_attn_func(query.transpose(1, 2),
+                                                key[:, :, :sample_size, :].transpose(1, 2),
+                                                value[:, :, :sample_size, :].transpose(1, 2))
+        attn_block = attn_block.transpose(1, 2)
+        attn_block = attn_block.view(batch_size, head_size, query_sorted.shape[2], -1)
+        attn_sample = attn_sample.transpose(1, 2)
+        lse_block = lse_block[:, :, :query_sorted.shape[2]]
+        lse_block = lse_block.view(batch_size, head_size, query_sorted.shape[2], -1)
+        flash_attn_block = indexing(attn_block, query_sort_idx_inv)+attn_sample
+        lse_block = indexing(lse_block, query_sort_idx_inv)
+        attn, lse = add_self_attentions(flash_attn_block, lse_block, attn_sample, lse_sample.unsqueeze(-1))
+        t1 = time.time()
+        check_memory()
+        print('the runtime of flash attention with permutation and indexing of queries:', t1-t0)
+
+        # torch lse computation
+        qk = query_split_per_block @ key_split_per_block.transpose(-1, -2) / math.sqrt(dim)
+        lse_block_torch = torch.logsumexp(qk, dim=-1, keepdim=True)
+        lse_block_torch = lse_block_torch.view(batch_size, head_size, query_sorted.shape[2], -1)
+        lse_block_torch = indexing(lse_block_torch, query_sort_idx_inv).squeeze(-1)
+        lse_sample_torch = torch.logsumexp(
+            query @ key[:, :, :sample_size, :].transpose(-1, -2) / math.sqrt(dim),
+            dim=-1,
+            keepdim=True
+        ).squeeze(-1)
+        lse_torch = (lse_sample_torch.exp() + lse_block_torch.exp()).log().to(dtype=lse_block_torch.dtype)
+        print('diff between lse with sample and without: ', (lse_block_torch - lse_torch).norm(), lse_torch.norm())
+        print('error flash attention:', (lse - lse_torch).norm(), lse_torch.norm())
+
+        # compute attention kernel which permutes queries in triton
+        check_memory(0)
+        t2 = time.time()
+        attn_triton, lse_triton = hyper_attn_func(
+            query.transpose(1, 2),
+            key.transpose(1, 2),
+            value.transpose(1, 2),
+            query_sort_idx.transpose(1, 2),
+            key_sort_idx.transpose(1, 2),
+            block_size,
+            sample_size,
+        )
+        attn_triton = attn_triton.transpose(1, 2)
+        t3 = time.time()
+        check_memory()
+
+        print('the runtime of hyper attention:', t3 - t2)
+
+        print('diff lse hyper_attention and flash with indexing and permutation: ', (lse - lse_triton).norm(), lse.norm())
+
+        print('error hyper attention lse: ', (lse_triton - lse_torch).norm(), lse_torch.norm())
+
+        # check if dimension of V can be different from that of Q and K
+        value_small = value[:, :, :, :dim//2].clone()
+        attn_triton_unequal_dim, lse_triton_unequal_dim = hyper_attn_func(
+            query.transpose(1, 2),
+            key.transpose(1, 2),
+            value_small.transpose(1, 2),
+            query_sort_idx.transpose(1, 2),
+            key_sort_idx.transpose(1, 2),
+            block_size,
+            sample_size,
+        )
+        attn_triton_unequal_dim = attn_triton_unequal_dim.transpose(1, 2)
+
+        print('testing unequal dimension for V compared to Q, K')
+        print((attn_triton[:, :, :, :dim//2] - attn_triton_unequal_dim).norm())
 
     def test_gradient(self):
         dtype = torch.bfloat16
@@ -251,8 +245,6 @@ class MyTestCase(unittest.TestCase):
 
         print('difference of values, torch vs hyper:')
         print((v_grad_torch - v_grad_hyper).norm(), v_grad_torch.norm(), v_grad_hyper.norm())
-
-
 
 
 if __name__ == '__main__':
